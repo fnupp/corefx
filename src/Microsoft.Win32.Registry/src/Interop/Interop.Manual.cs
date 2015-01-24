09323d3f369705fb54819997e9c55ba9f9719cdb
fnupp <falko.nuppnau@gmail.com>
2015-01-24 13:47:12 +0100

    test

--- src/Microsoft.Win32.Registry/src/Interop/Interop.Manual.cs
+++ src/Microsoft.Win32.Registry/src/Interop/Interop.Manual.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+    //etst
 using Microsoft.Win32;
 using Microsoft.Win32.SafeHandles;
 using System;
